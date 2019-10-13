b8354b241e1302b15a499a8aaa931ea45997fd3c
spasi8998 <gconte975@gmail.com>
2019-10-13 17:50:27 +0200

    Ciclo For

---
+++ esercizio x lunedi.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+      
+      int voto [ 10 ];
+       int i;
+	    float Media;
+   for ( i=0; i < 10; i++ ){
+         	
+         	cout << "Inserisci il " << i << " numero" << endl;
+         	 cin >> voto[i];
